4430d93a4a85cbfbaebc91633b89446c12cdeb8a
michael maryann <michaelmaryanne@gmail.com>
2023-02-17 23:44:11 -0800

    variables c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,11 +7,12 @@
  */
 int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}	
-	putchar('\n');
-       	return (0);
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}	
+putchar('\n');
+return (0);
 }
